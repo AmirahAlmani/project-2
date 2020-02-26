69b8324c83e4191699ec2ea4d9e661bfa1d930a9
Amirah Almani <amirahalmani@gmail.com>
2020-02-26 16:05:26 +0300

    Updates

---
+++ precache-manifest.6b471791126a4424516a2d06cebe990c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1db2d6e2485542b71bf6bf4df702eee",
+    "url": "/project-2/index.html"
+  },
+  {
+    "revision": "7d4357ea28fb390bf91e",
+    "url": "/project-2/static/css/main.5b8c7d62.chunk.css"
+  },
+  {
+    "revision": "63b604ec85f5a8fc9b70",
+    "url": "/project-2/static/js/2.861ba1a1.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/project-2/static/js/2.861ba1a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d4357ea28fb390bf91e",
+    "url": "/project-2/static/js/main.d936f74c.chunk.js"
+  },
+  {
+    "revision": "e6092009734313a02f58",
+    "url": "/project-2/static/js/runtime-main.cb88c370.js"
+  }
+]);
